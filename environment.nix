7bfa7abe987c3e2d6cbfacdbcb1d52142781e250
Jessica Fileto <jessicalima@duck.com>
2024-10-26 20:23:24 -0300

    fix some stuff

--- environment.nix
+++ environment.nix
@@ -1,51 +1,54 @@
-{ config, pkgs, inputs, ... }: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   programs.firefox.enable = true;
   environment.systemPackages = with pkgs; [
-     vim
-     zsh
-     htop
-     git
-     fzf
-     bat
-     eza
-     tlrc
-     thefuck
-     lazygit
-     oh-my-zsh
-     fd
-     fzf-git-sh
-     delta
-     neovim
-     zsh-autosuggestions
-     zsh-syntax-highlighting
-     gh
-     ethtool
-     ripgrep
-     vscode
-     wezterm
-     autojump
-     asdf
-     wget
-     tcpdump
-     podman
-     podman-desktop
-     libgcc
-     jetbrains.idea-community-bin
-     just
-     colorls
-     gcc
-     helix
-     asdf-vm
-     podman-compose
-     inputs.nixvim.packages.x86_64-linux.default
+    vim
+    zsh
+    htop
+    git
+    fzf
+    bat
+    eza
+    tlrc
+    thefuck
+    lazygit
+    oh-my-zsh
+    fd
+    fzf-git-sh
+    delta
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    gh
+    ethtool
+    ripgrep
+    vscode
+    wezterm
+    autojump
+    asdf
+    wget
+    tcpdump
+    podman
+    podman-desktop
+    libgcc
+    jetbrains.idea-community-bin
+    just
+    colorls
+    gcc
+    helix
+    asdf-vm
+    podman-compose
+    inputs.nixvim.packages.x86_64-linux.default # neovim config from github
   ];
 
- environment.variables.EDITOR = "nvim";
- virtualisation.containers.enable = true;
- virtualisation = {
+  environment.variables.EDITOR = "nvim";
+  virtualisation.containers.enable = true;
+  virtualisation = {
     podman = {
-        enable = true;
-        defaultNetwork.settings.dns_enabled = true;
-      };
+      enable = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
   };
 }
