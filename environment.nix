f070d9c7499f2b84387c8fd58b2454b2816557b7
Jessica Fileto <jessicalima@duck.com>
2025-02-19 15:05:02 -0300

    adds spotify

--- environment.nix
+++ environment.nix
@@ -44,6 +44,7 @@
     # inputs.nixvim.packages.x86_64-linux.default # neovim config from github #removing to waiting fix
     lazydocker
     onlyoffice-bin
+    spotify
   ];
 
   environment.variables.EDITOR = "nvim";
