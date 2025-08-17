576756eafc762e9f2f1c56606e1ddf727cdee8b8
Jessica Fileto <jessicalima@duck.com>
2025-08-16 23:23:15 -0300

    again trying to fix flake

--- flake.nix
+++ flake.nix
@@ -3,26 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixvim.url = "github:daniloraisi/nixvim";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: {
     nixosConfigurations.jessicafileto = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
 
       modules = [
         ./configuration.nix
 
         {
-          # Habilita unfree globalmente
           nixpkgs.config.allowUnfree = true;
 
-          # Overlay para acessar pkgs.unstable
           nixpkgs.overlays = [
             (final: prev: {
               unstable = import nixpkgs-unstable {
                 system = "x86_64-linux";
-                config.allowUnfree = true;  # <- aqui garante para unstable
+                config.allowUnfree = true;
               };
             })
           ];
